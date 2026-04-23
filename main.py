9bf5172bd3ce3136eb2d1882db10ede10ad99e18
inoahide-ai <inoahide@gmail.com>
2026-04-23 16:05:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from data.market_data import get_market_status
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Trading system is running 🚀"
+
+@app.route("/market")
+def market():
+    return get_market_status()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
